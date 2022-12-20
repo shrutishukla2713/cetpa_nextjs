62289361dd939321c735a4f0e9b7ee9ee7686ab3
shruti2713 <shruti.shukla@altudo.co>
2022-12-20 23:08:41 +0530

    test@0

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,27 @@
-import '../styles/globals.css'
-import Layout from "../Common/Layout";
+import Layout from '../common/Layout';
+import '../styles/globals.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Roboto} from '@next/font/google';
 
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 
 // function MyApp({ Component, pageProps }) {
 //   return <Component {...pageProps} />
 // }
 
 function MyApp({ Component, pageProps }) {
-  console.log('pageProps',pageProps)
-  return(
-    <Layout footerstatus={pageProps.footerstatus}>
-       <Component {...pageProps} />
-    </Layout>
-  )
+  console.log('pageProps', pageProps);
+  return (
+      <Layout footerstatus={pageProps.footerstatus}>
+          <div className={roboto.className}>
+            <Component {...pageProps} />
+          </div>
+      </Layout>
+    )
 }
 
 export default MyApp
